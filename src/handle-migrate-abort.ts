6b379d3c110cfddee54da4648e797539c3511348
metalwarrior665 <lukaskrivka@gmail.com>
2024-12-12 19:42:51 +0100

    docs: add logs around persit

--- src/handle-migrate-abort.ts
+++ src/handle-migrate-abort.ts
@@ -1,4 +1,4 @@
-import { Actor } from 'apify';
+import { Actor, log } from 'apify';
 import type { CheerioCrawler } from 'crawlee';
 import { sleep, KeyValueStore, BasicCrawler } from 'crawlee';
 
@@ -18,6 +18,7 @@ const persistState = async (crawler: CheerioCrawler) => {
     // 1. Not race condition with Crawlee's internal persist state, instead cleanly overwrite it
     // 2. Allow some pending requests to finish, Crawlee doesn't start new ones after event is emitted
     await sleep(10_000);
+    log.info('Waited 10 seconds before persisting state, now storing state');
 
     // We will await persisting of the components concurrently to minimize the latency
     await Promise.all([
@@ -29,14 +30,19 @@ const persistState = async (crawler: CheerioCrawler) => {
 
 export const setupMigrateAbortHandlers = async (crawler: CheerioCrawler) => {
     Actor.on('migrating', async () => {
+        log.info('[migrating]: Registered event, starting state persist with initial 10s sleep');
         await persistState(crawler);
+        log.info('[migrating]: State persist finished, rebooting actor');
         // Reboot usually takes less than 100 ms between calling it and Node.js exiting
         // That's not perfect but chance of mismatch is very low
         const runId = Actor.config.get('actorRunId');
         await Actor.apifyClient.run(runId!).reboot();
+        log.info('[migrating]: Reboot called, waiting to be killed');
     });
     Actor.on('aborting', async () => {
+        log.info('[aborting]: Registered event, starting state persist with initial 10s sleep');
         await persistState(crawler);
+        log.info('[aborting]: State persist finished, shutting down');
         // Shut down immediately after state is persisted
         process.exit(0);
     });
